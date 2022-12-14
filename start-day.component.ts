4de7b4baa8616c42ee44b91db2eec09002bee030
kaustubhnegiOC <118799800+kaustubhnegiOC@users.noreply.github.com>
2022-12-14 19:31:34 +0530

    Add files via upload

---
+++ start-day.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SenderService } from '../sender.service';
+
+@Component({
+  selector: 'app-start-day',
+  templateUrl: './start-day.component.html',
+  styleUrls: ['./start-day.component.css']
+})
+export class StartDayComponent {
+
+    private from1 = "Hello from StartDayComponent";
+
+    constructor(private router: Router,
+                private service: SenderService){
+      
+    }
+    now = new Date()
+
+    goto2(){
+      this.service.variable1 = this.from1;
+    }
+ 
+} 
